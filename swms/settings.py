d1498a0ba9b1ea78501e4d8598707e9fe20ad556
Ali-mahirizi-abdalla <alimahrez744@gmail.com>
2026-03-28 00:04:36 +0300

    Restore original admin (March 12th) and sync static paths

--- swms/settings.py
+++ swms/settings.py
@@ -220,7 +220,7 @@ USE_TZ = True
 # STATIC FILES
 # ============================================
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'hms/static')]
 
 
