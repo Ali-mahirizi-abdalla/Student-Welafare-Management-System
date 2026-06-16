d4135fcef4635d4e0f0cdaf01890480621a56546
Ali-mahirizi-abdalla <alimahrez744@gmail.com>
2026-06-16 13:08:48 +0300

    Add professional landing page with student and staff portals

--- swms/urls.py
+++ swms/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 from subscription.views import intasend_webhook
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('library/', include('library.urls')),
